b94aa7489ac4556c1b3fbedf97f4ec5080d6f08d
Victor Mureithi <victormureithig@gmail.com>
2024-03-02 15:38:05 +0300

    Implement whatsapp message feature

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,24 +1,105 @@
+// import React, { useState } from "react";
+// import "./contact.css";
+// import { MdOutlineEmail } from "react-icons/md";
+// // import emailjs from "emailjs-com";
+
+// const Contact = () => {
+//   const [emailIsSent, setEmailIsSent] = useState(false);
+
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+
+//     const { name, message } = e.target.elements;
+
+//     const mailtoLink = `mailto:victormureithig@gmail.com?subject=New Contact Form Submission&body=Name: ${name.value}%0AMessage: ${message.value}`;
+
+//     window.location.href = mailtoLink;
+
+//     // If you want to set emailIsSent to true after sending the email, uncomment the next line
+//     setTimeout(() => {
+//       setEmailIsSent(true);
+//     }, 10000);
+//   };
+
+//   return (
+//     <section id="contact">
+//       <h5>Get In Touch</h5>
+//       <h2>Contact Me</h2>
+//       <div className="container contact__container">
+//         <div className="contact__options">
+//           <article className="contact__option">
+//             <MdOutlineEmail className="contact__option-icon" />
+//             <h4>Email</h4>
+//             <h5>victormureithig@gmail.com</h5>
+//             <a href="mailto:victormureithig@gmail.com">Send a message</a>
+//           </article>
+//           <article className="contact__option">
+//             <MdOutlineEmail className="contact__option-icon" />
+//             <h4>LinkedIn</h4>
+//             <h5>Victor Mureithi</h5>
+//             <a href="https://www.linkedin.com/in/victor-mureithi-97b469294/">
+//               Send a message
+//             </a>
+//           </article>
+//         </div>
+//         {/* END OF CONTACT OPTIONS */}
+//         {emailIsSent ? (
+//           <h2 id="Contact__sent-message">
+//             Your Message was successfully sent!
+//           </h2>
+//         ) : (
+//           <form onSubmit={sendEmail}>
+//             <input
+//               type="text"
+//               name="name"
+//               placeholder="Your Full Name"
+//               required
+//             />
+//             <input
+//               type="email"
+//               name="email"
+//               placeholder="Your Email"
+//               required
+//             />
+//             <textarea
+//               name="message"
+//               rows="7"
+//               placeholder="Your Message"
+//             ></textarea>
+//             <button type="submit" className="btn btn-primary">
+//               Send Message
+//             </button>
+//           </form>
+//         )}
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Contact;
 import React, { useState } from "react";
 import "./contact.css";
 import { MdOutlineEmail } from "react-icons/md";
-// import emailjs from "emailjs-com";
+import { BsLinkedin } from "react-icons/bs";
 
 const Contact = () => {
-  const [emailIsSent, setEmailIsSent] = useState(false);
+  const [messageIsSent, setMessageIsSent] = useState(false);
 
-  const sendEmail = (e) => {
+  const sendMessage = (e) => {
     e.preventDefault();
 
     const { name, message } = e.target.elements;
 
-    const mailtoLink = `mailto:victormureithig@gmail.com?subject=New Contact Form Submission&body=Name: ${name.value}%0AMessage: ${message.value}`;
+    const whatsappLink = `https://wa.me/254113445466?text=Name:%20${name.value}%0AMessage:%20${message.value}`;
 
-    window.location.href = mailtoLink;
+    const whatsappWindow = window.open(whatsappLink, "_blank");
 
-    // If you want to set emailIsSent to true after sending the email, uncomment the next line
-    setTimeout(() => {
-      setEmailIsSent(true);
-    }, 10000);
+    const checkWindowClosed = setInterval(() => {
+      if (whatsappWindow.closed) {
+        clearInterval(checkWindowClosed);
+        setMessageIsSent(true);
+      }
+    }, 1000);
   };
 
   return (
@@ -34,7 +115,7 @@ const Contact = () => {
             <a href="mailto:victormureithig@gmail.com">Send a message</a>
           </article>
           <article className="contact__option">
-            <MdOutlineEmail className="contact__option-icon" />
+            <BsLinkedin className="contact__option-icon" />
             <h4>LinkedIn</h4>
             <h5>Victor Mureithi</h5>
             <a href="https://www.linkedin.com/in/victor-mureithi-97b469294/">
@@ -43,28 +124,23 @@ const Contact = () => {
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        {emailIsSent ? (
+        {messageIsSent ? (
           <h2 id="Contact__sent-message">
             Your Message was successfully sent!
           </h2>
         ) : (
-          <form onSubmit={sendEmail}>
+          <form onSubmit={sendMessage}>
             <input
               type="text"
               name="name"
               placeholder="Your Full Name"
               required
             />
-            <input
-              type="email"
-              name="email"
-              placeholder="Your Email"
-              required
-            />
             <textarea
               name="message"
               rows="7"
               placeholder="Your Message"
+              required
             ></textarea>
             <button type="submit" className="btn btn-primary">
               Send Message
