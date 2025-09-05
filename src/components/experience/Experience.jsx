0c31116054bd29403c8b05f2a067ad15cb5da0ec
Victor Mureithi <victormureithig@gmail.com>
2025-09-05 15:19:05 +0300

    Update backend development mongo Db to experienced

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -93,7 +93,7 @@ const Experience = () => {
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Mongo DB</h4>
-                <small className="text-light">Basic</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className="experience__details">
